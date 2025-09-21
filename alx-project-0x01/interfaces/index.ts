f3fe27194f3a67b480c14338d8c19bbf360fd1f8
amarisodiq <amarisodiq@gmail.com>
2025-09-21 22:10:35 +0100

    fix bug

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -46,32 +46,33 @@ export interface UserProps {
     onSubmit: (post: PostData) => void;
   }
 
-  export interface UserData {
-    id: number;
+
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
     name: string;
-    username: string;
-    email: string;
-    address: {
-      street: string;
-      suite: string;
-      city: string;
-      zipcode: string;
-      geo: {
-        lat: string;
-        lng: string;
-      };
-    };
-    phone: string;
-    website: string;
-    company: {
-      name: string;
-      catchPhrase: string;
-      bs: string;
-    };
-  }
-  
-  export interface UserModalProps {
-    isOpen: boolean;
-    onClose: () => void; 
-    onSave: (user: UserData) => void; 
-  }
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface UserModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (post: UserProps) => void;
+}
